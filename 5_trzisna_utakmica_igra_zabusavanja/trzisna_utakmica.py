a1176315c8fe1139b06cda55574eb656cfd5a7c0
Filip <filipeng97@gmail.com>
2020-12-27 17:12:10 +0100

    Domaci 5 -edit

--- 5_trzisna_utakmica_igra_zabusavanja/trzisna_utakmica.py
+++ 5_trzisna_utakmica_igra_zabusavanja/trzisna_utakmica.py
@@ -6,22 +6,24 @@ DISCOUNT = 1 - 0.1
 
 
 D = 1000
-A = [0, 0]
+A = [[0], [0]]
 
 C  = [ 100, 95 ]
 def c(i):
     var_material_price = C[i] *  uniform(0.9, 1.1)
-    return var_material_price if A[i] < 1000 else var_material_price * DISCOUNT
+    return var_material_price if A[i][-1] < 1000 else var_material_price * DISCOUNT
 
 def calculate_a(i):
-    return round((D - sum(A) + A[i] - c(i)) / 2)
+    a = round((D - sum(A[0]) - sum(A[1]) + sum(A[i]) - c(i)) / 2)
+    return a if a > 0 else 0
 
 
 def p(i):
-    return D - A[i]
+    return D - A[i][-1]
 
 def u(i):
-    return round(p(i) * A[i] - c(i)) 
+    u =  round(p(i) * A[i][-1] - c(i)) 
+    return u if u > 0 else 0
 
 
 if __name__ == "__main__":
@@ -33,8 +35,8 @@ if __name__ == "__main__":
         print('a1= ', a1)
         print('a2= ', a2)
 
-        A[0] += a1
-        A[1] += a2
+        A[0].append(a1)
+        A[1].append(a2)
 
         u1 = u(0)
         u2 = u(1)
